28f2d9513f216ece27596bbf5131a047ce0f28b6
Romain T <romain-thd+github@protonmail.com>
2021-06-07 11:47:27 +0200

    change: Validation

--- src/helper/validation.ts
+++ src/helper/validation.ts
@@ -1,25 +1,40 @@
 import Joi from "joi";
 import Language from "helper/language";
+import {ExpressJoiInstance} from "express-joi-validation";
+
+const validator: ExpressJoiInstance = require("express-joi-validation").createValidator({
+    passError: true
+});
 
 /**
  * Validation des entrées
  */
 export default abstract class Validation {
-    /**
-     * Login
-     */
-    public static formLoginSchema = Joi.object({
-        username: Joi.string().alphanum().min(3).max(32).required().messages({
-            "string.empty": Language.get("validation.login.username.empty"),
-            "string.min": Language.get("validation.login.username.short"),
-            "string.max": Language.get("validation.login.username.long"),
-            "any.required": Language.get("validation.login.username.required")
-        }),
-        password: Joi.string().min(8).required().messages({
-            "string.empty": Language.get("validation.login.password.empty"),
-            "string.min": Language.get("validation.login.password.short"),
-            "string.max": Language.get("validation.login.password.long"),
-            "any.required": Language.get("validation.login.password.required")
-        }),
-    });
+    //==================================================================================================================
+    // Schémas JOI
+    //==================================================================================================================
+
+    static object(obj: Object = {}) {
+        return Joi.object(obj);
+    }
+
+    static string() {
+        return Joi.string();
+    }
+
+    //==================================================================================================================
+    // Méthodes HTTP
+    //==================================================================================================================
+
+    static get(schema: Joi.AnySchema) {
+        return validator.params(schema);
+    }
+
+    static query(schema: Joi.AnySchema) {
+        return validator.query(schema);
+    }
+
+    static post(schema: Joi.AnySchema) {
+        return validator.body(schema);
+    }
 }
