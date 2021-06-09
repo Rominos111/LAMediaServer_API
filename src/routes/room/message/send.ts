46c93cf388be94e6550a5ecd3b8ab4b9acb4bf70
Romain T <romain-thd+github@protonmail.com>
2021-06-09 14:54:34 +0200

    new: Envoi de message

--- src/routes/room/message/send.ts
+++ src/routes/room/message/send.ts
@@ -1,5 +1,27 @@
-import express from "express";
+import APIRequest from "helper/APIRequest";
+import Language from "helper/language";
+import RocketChatRequest from "helper/request";
+import Validation from "helper/validation";
 
-let router = express.Router();
+const schema = Validation.object({
+    token: Validation.jwt().required().messages({
+        "any.required": Language.get("validation.token.required"),
+    }),
+    roomId: Validation.string().required().messages({
+        "any.required": Language.get("validation.id.required"),
+    }),
+    message: Validation.string().trim().min(1).max(2_000).required().messages({
+        "string.min": Language.get("validation.message.short"),
+        "string.max": Language.get("validation.message.long"),
+        "string.trim": Language.get("validation.message.short"),
+        "string.empty": Language.get("validation.message.short"),
+        "any.required": Language.get("validation.message.required"),
+    }),
+});
 
-module.exports = router;
+module.exports = APIRequest.post(schema, (req, res) => {
+    RocketChatRequest.request("POST", "/chat.sendMessage", req.body.token, res, {
+        rid: req.body.roomId,
+        msg: req.body.message.trim(),
+    });
+});
