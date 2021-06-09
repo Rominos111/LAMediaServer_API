441bf70abbc72e15a86a622e50eac3aabc381d4a
Romain T <romain-thd+github@protonmail.com>
2021-06-09 16:07:32 +0200

    new: Réactions

--- src/routes/room/message/react.ts
+++ src/routes/room/message/react.ts
@@ -1,5 +1,30 @@
-import express from "express";
+import APIRequest from "helper/APIRequest";
+import Language from "helper/language";
+import RocketChatRequest from "helper/request";
+import Validation from "helper/validation";
 
-let router = express.Router();
+const schema = Validation.object({
+    token: Validation.jwt().required().messages({
+        "any.required": Language.get("validation.token.required"),
+    }),
+    messageId: Validation.string().required().messages({
+        "any.required": Language.get("validation.id.required"),
+    }),
+    emojiName: Validation.string().required().messages({
+        "string.empty": Language.get("validation.emoji.required"),
+        "any.required": Language.get("validation.emoji.required"),
+    }),
+    operation: Validation.string().valid("set", "clear").required().messages({
+        "any.only": Language.get("validation.reaction-operation.invalid"),
+        "any.required": Language.get("validation.reaction-operation.required"),
+    }),
+});
 
-module.exports = router;
+module.exports = APIRequest.post(schema, (req, res) => {
+    console.log(req.body.operation);
+    RocketChatRequest.request("POST", "/chat.react", req.body.token, res, {
+        emoji: req.body.emojiName.trim(),
+        messageId: req.body.messageId,
+        shouldReact: req.body.operation.toLowerCase() === "set",
+    });
+});
