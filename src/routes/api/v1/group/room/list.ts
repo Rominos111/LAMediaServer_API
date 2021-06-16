38c768eb69ee1c194d42f18fefefc21f186c8263
Romain T <romain-thd+github@protonmail.com>
2021-06-16 19:09:27 +0200

    fix: Erreur 404 générée plutôt qu'une erreur 400

--- src/routes/api/v1/group/room/list.ts
+++ src/routes/api/v1/group/room/list.ts
@@ -15,6 +15,7 @@ const schema = Validation.object({
 });
 
 module.exports = APIRequest.get(schema, (req, res) => {
+    // FIXME: Utiliser `.query` (cf. `app.js`)
     RocketChatRequest.request(RequestMethod.GET, "/rooms.getDiscussions", req, res, {
         roomId: req.body.groupRoomId,
         count: 0,
@@ -26,5 +27,12 @@ module.exports = APIRequest.get(schema, (req, res) => {
         }
 
         return APIResponse.fromSuccess(rooms);
+    }, (r, data) => {
+        if (r.status === 400 && data.errorType === "error-room-not-found") {
+            return APIResponse.fromFailure("Room not found", 404);
+        } else {
+            console.debug(data);
+            return APIResponse.fromFailure(r.statusText, r.status);
+        }
     });
 });
