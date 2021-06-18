089d45145d515af3a68a212418a100589fbb8675
Romain T <romain-thd+github@protonmail.com>
2021-06-18 11:42:29 +0200

    fix: Groupes imaginaires
    Les teams Rocket.chat supprimées apparaissent toujours dans leur API REST.
    La team est listée, mais sa room n'existe plus.
    Impossible de la supprimer donc, puisqu'elle n'a déjà plus de room, donc une erreur est lancée par l'API.

--- src/routes/api/v1/group/list.ts
+++ src/routes/api/v1/group/list.ts
@@ -6,13 +6,34 @@ import {Group} from "model/group";
 module.exports = APIRequest.get(null, (req, res) => {
     RocketChatRequest.request("GET", "/teams.list", req, res, {
         count: 0,
-    }, (r, data) => {
+    }, async (r, data) => {
         const groups: Group[] = [];
+        const promises: Promise<void>[] = [];
 
         for (const team of data.teams) {
-            groups.push(Group.fromFullObject(team));
+            let group = Group.fromFullObject(team);
+
+            const p = RocketChatRequest.request("GET", "/rooms.getDiscussions", req, null, {
+                roomId: group.roomId,
+                count: 1,
+            }, () => {
+                groups.push(group);
+                return null;
+            }, (r, data) => {
+                if (r.status !== 400 || data.errorType !== "error-room-not-found") {
+                    console.debug("/group/list:", r.status, r.statusText, data)
+                }
+
+                return null;
+            });
+
+            promises.push(p);
+        }
+
+        for (const p of promises) {
+            await p;
         }
 
         return APIResponse.fromSuccess(groups);
-    });
+    }).then();
 });
