799c58b5597133f296840c17885b9a99071ced38
Romain T <romain-thd+github@protonmail.com>
2021-06-21 13:40:31 +0200

    fix: Ordre des groupes inconsistant

--- src/routes/api/v1/group/list.ts
+++ src/routes/api/v1/group/list.ts
@@ -7,17 +7,19 @@ module.exports = APIRequest.get(null, (req, res) => {
     RocketChatRequest.request("GET", "/teams.list", req, res, {
         count: 0,
     }, async (r, data) => {
-        const groups: Group[] = [];
+        const groups: (Group|null)[] = [];
         const promises: Promise<void>[] = [];
 
-        for (const team of data.teams) {
-            let group = Group.fromFullObject(team);
+        for (let i = 0; i < data.teams.length; ++i) {
+            const team = data.teams[i];
+            const group = Group.fromFullObject(team);
+            groups.push(null);
 
             const p = RocketChatRequest.request("GET", "/rooms.getDiscussions", req, null, {
                 roomId: group.roomId,
                 count: 1,
             }, () => {
-                groups.push(group);
+                groups[i] = group;
                 return null;
             }, (r, data) => {
                 if (r.status !== 400 || data.errorType !== "error-room-not-found") {
@@ -34,6 +36,7 @@ module.exports = APIRequest.get(null, (req, res) => {
             await p;
         }
 
-        return APIResponse.fromSuccess(groups);
+        const groupsFiltered = groups.filter(group => group !== null);
+        return APIResponse.fromSuccess(groupsFiltered);
     }).then();
 });
