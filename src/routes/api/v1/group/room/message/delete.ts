d5634b864b93f2726a133ee854f4406ba76e1490
Romain T <romain-thd+github@protonmail.com>
2021-06-21 11:15:19 +0200

    new: Suppression d'un message

---
+++ src/routes/api/v1/group/room/message/delete.ts
@@ -0,0 +1,30 @@
+import {APIRequest} from "helper/APIRequest";
+import {APIResponse} from "helper/APIResponse";
+import {Language} from "helper/language";
+import {RocketChatRequest} from "helper/RocketChatRequest";
+import {Validation} from "helper/validation";
+
+const schema = Validation.object({
+    roomId: Validation.string().required().messages({
+        "any.required": Language.get("validation.id.required"),
+    }),
+    messageId: Validation.string().required().messages({
+        "any.required": Language.get("validation.id.required"),
+    }),
+});
+
+module.exports = APIRequest.delete(schema, (req, res) => {
+    RocketChatRequest.request("POST", "/chat.delete", req, res, {
+        roomId: req.body.roomId,
+        msgId: req.body.messageId,
+        asUser: true,
+    }, (_r, data) => {
+        if (data.success === true) {
+            return APIResponse.fromSuccess();
+        } else {
+            return APIResponse.fromFailure();
+        }
+    }, (_r, _data) => {
+        return APIResponse.fromFailure();
+    }).then();
+});
