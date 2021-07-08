65b008eab63a985383096bb3989c4eb4ed721723
Romain T <romain-thd+github@protonmail.com>
2021-07-08 14:25:20 +0200

    new: Modification d'un message

--- src/routes/api/v1/group/room/message/edit.rest.ts
+++ src/routes/api/v1/group/room/message/edit.rest.ts
@@ -3,10 +3,15 @@
  */
 
 import {APIRequest} from "helper/APIRequest";
+import {APIResponse} from "helper/APIResponse";
 import {Language} from "helper/language";
 import {RequestMethod} from "helper/requestMethod";
 import {RocketChatRequest} from "helper/rocketChatRequest";
 import {Validation} from "helper/validation";
+import {
+    Message,
+    RawFullMessage,
+} from "model/message";
 
 const schema = Validation.object({
     message: Validation.string().trim().min(1).max(2_000).required().messages({
@@ -30,7 +35,16 @@ module.exports = APIRequest.put(schema, true, async (req, res, auth) => {
         roomId: req.body.roomId,
         text: req.body.message,
     }, (r, data) => {
-        console.log(r);
-        return null;
+        return APIResponse.fromSuccess(Message.fromFullMessage(data.message as RawFullMessage, auth?.userId as string));
+    }, (r, data) => {
+        if (r.status === 400) {
+            if (r.statusText.toLowerCase().includes("no message found")) {
+                return APIResponse.fromFailure(Language.get("validation.not-found"), 404);
+            } else if (r.statusText.toLowerCase().includes("room id provided does not match")) {
+                return APIResponse.fromFailure(Language.get("validation.no-match-room"), 404);
+            }
+        }
+
+        return APIResponse.fromFailure(r.statusText, data.error as string);
     });
 });
