5d0dc1b615f035af7e5138af344ed5c6e8ad82a7
Romain T <romain-thd+github@protonmail.com>
2021-07-13 16:35:02 +0200

    new: Envoi d'un message via WebSocket

--- src/routes/api/v1/group/room/message/send.rest.ts
+++ src/routes/api/v1/group/room/message/send.rest.ts
@@ -4,29 +4,14 @@
 
 import {APIRequest} from "helper/APIRequest";
 import {APIResponse} from "helper/APIResponse";
-import {Language} from "helper/language";
 import {RocketChatRequest} from "helper/rocketChatRequest";
-import {Validation} from "helper/validation";
 import {
     Message,
     RawFullMessage,
 } from "model/message";
+import {schema_sendMessage} from "./send.shared";
 
-const schema = Validation.object({
-    // FIXME: Set la limite en variable d'environnement ?
-    message: Validation.string().trim().min(1).max(2_000).required().messages({
-        "any.required": Language.get("validation.message.required"),
-        "string.empty": Language.get("validation.message.short"),
-        "string.max": Language.get("validation.message.long"),
-        "string.min": Language.get("validation.message.short"),
-        "string.trim": Language.get("validation.message.short"),
-    }),
-    roomId: Validation.string().required().messages({
-        "any.required": Language.get("validation.id.required"),
-    }),
-});
-
-module.exports = APIRequest.post(schema, true, async (req, res, auth) => {
+module.exports = APIRequest.post(schema_sendMessage, true, async (req, res, auth) => {
     await RocketChatRequest.request("POST", "/chat.sendMessage", auth, res, {
         message: {
             msg: req.body.message.trim(),
