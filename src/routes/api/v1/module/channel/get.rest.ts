7787501936aeafb54feb8adf9bbce829e6b2a130
Romain T <romain-thd+github@protonmail.com>
2021-07-19 10:21:59 +0200

    new: Récupération d'un canal

--- src/routes/api/v1/module/channel/get.rest.ts
+++ src/routes/api/v1/module/channel/get.rest.ts
@@ -1,3 +1,18 @@
 import {APIRequest} from "helper/APIRequest";
+import {APIResponse} from "helper/APIResponse";
+import {RequestMethod} from "helper/requestMethod";
+import {RocketChatRequest} from "helper/rocketChatRequest";
+import {Validation} from "helper/validation";
+import {RawChannel} from "model/channel";
 
-module.exports = APIRequest.wip();
+const schema = Validation.object({
+    channelId: Validation.string().required(),
+});
+
+module.exports = APIRequest.get(schema, true, async (req, res, auth) => {
+    await RocketChatRequest.request(RequestMethod.GET, "/rooms.info", auth, res, {
+        roomId: req.query.channelId,
+    }, (r, data) => {
+        return APIResponse.fromSuccess(data.room as RawChannel);
+    });
+});
