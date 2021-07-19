dd382a7da106b87cfc7dcfdf14bee9e9f06c3ef4
Romain T <romain-thd+github@protonmail.com>
2021-07-19 09:44:58 +0200

    new: Liste des canaux par WebSocket

--- src/routes/api/v1/module/channel/list.rest.ts
+++ src/routes/api/v1/module/channel/list.rest.ts
@@ -4,43 +4,23 @@
 
 import {APIRequest} from "helper/APIRequest";
 import {APIResponse} from "helper/APIResponse";
-import {Language} from "helper/language";
+import {Authentication} from "helper/authentication";
+import {HTTPStatus} from "helper/requestMethod";
 import {
-    HTTPStatus,
-    RequestMethod,
-} from "helper/requestMethod";
-import {RocketChatRequest} from "helper/rocketChatRequest";
-import {Validation} from "helper/validation";
-import {
-    Channel,
-    RawChannel,
-} from "model/channel";
+    listChannels,
+    schema_listModules,
+} from "./list.shared";
 
-const schema = Validation.object({
-    moduleRoomId: Validation.string().required().messages({
-        "any.required": Language.get("validation.id.required"),
-    }),
-});
-
-module.exports = APIRequest.get(schema, true, async (req, res, auth) => {
-    await RocketChatRequest.request(RequestMethod.GET, "/rooms.getDiscussions", auth, res, {
-        count: 0,
-        roomId: req.body.moduleRoomId,
-    }, (r, data) => {
-        const channels: Channel[] = [];
-
-        for (const rawChannel of data.discussions as RawChannel[]) {
-            channels.push(Channel.fromFullObject(rawChannel, r.currentUserId as string));
-        }
-
-        return APIResponse.fromSuccess({
+module.exports = APIRequest.get(schema_listModules, true, async (req, res, auth) => {
+    await listChannels(req.query.moduleRoomId as string, auth as Authentication, (channels) => {
+        APIResponse.fromSuccess({
             channels: channels,
-        });
+        }).send(res);
     }, (r, data) => {
         if (r.status === HTTPStatus.BAD_REQUEST && data.errorType === "error-room-not-found") {
-            return APIResponse.fromFailure("Channel not found", HTTPStatus.NOT_FOUND);
+            APIResponse.fromFailure("Channel not found", HTTPStatus.NOT_FOUND).send(res);
         } else {
-            return APIResponse.fromFailure(r.statusText, r.status);
+            APIResponse.fromFailure(r.statusText, r.status).send(res);
         }
     });
 });
---
+++ src/routes/api/v1/module/channel/list.shared.ts
@@ -0,0 +1,45 @@
+/**
+ * Liste les salons
+ */
+
+import {AxiosResponse} from "axios";
+import {Authentication} from "helper/authentication";
+import {Language} from "helper/language";
+import {RequestMethod} from "helper/requestMethod";
+import {
+    FailureData,
+    RocketChatRequest,
+} from "helper/rocketChatRequest";
+import {Validation} from "helper/validation";
+import {
+    Channel,
+    RawChannel,
+} from "model/channel";
+
+export const schema_listModules = Validation.object({
+    moduleRoomId: Validation.string().required().messages({
+        "any.required": Language.get("validation.id.required"),
+    }),
+});
+
+export async function listChannels(moduleRoomId: string,
+                                   auth: Authentication,
+                                   onSuccess: (channels: Channel[]) => void,
+                                   onFailure: (r: AxiosResponse, data: FailureData) => void): Promise<void> {
+    await RocketChatRequest.request(RequestMethod.GET, "/rooms.getDiscussions", auth, null, {
+        count: 0,
+        roomId: moduleRoomId,
+    }, (r, data) => {
+        const channels: Channel[] = [];
+
+        for (const rawChannel of data.discussions as RawChannel[]) {
+            channels.push(Channel.fromFullObject(rawChannel, r.currentUserId as string));
+        }
+
+        onSuccess(channels);
+        return null;
+    }, (r, data) => {
+        onFailure(r, data);
+        return null;
+    });
+}
