2e8d76f2679fae38e00ae0774739564471f25d34
Romain T <romain-thd+github@protonmail.com>
2021-07-19 10:16:41 +0200

    fix: Suppression d'un canal

--- src/routes/api/v1/module/channel/delete.rest.ts
+++ src/routes/api/v1/module/channel/delete.rest.ts
@@ -1,6 +1,10 @@
 import {APIRequest} from "helper/APIRequest";
+import {APIResponse} from "helper/APIResponse";
 import {Language} from "helper/language";
-import {RequestMethod} from "helper/requestMethod";
+import {
+    HTTPStatus,
+    RequestMethod,
+} from "helper/requestMethod";
 import {RocketChatRequest} from "helper/rocketChatRequest";
 import {Validation} from "helper/validation";
 
@@ -11,7 +15,15 @@ const schema = Validation.object({
 });
 
 module.exports = APIRequest.delete(schema, true, async (req, res, auth) => {
-    await RocketChatRequest.request(RequestMethod.POST, "/channels.delete", auth, res, {
+    await RocketChatRequest.request(RequestMethod.POST, "/groups.delete", auth, res, {
         roomId: req.body.channelId,
+    }, null, (r, data) => {
+        if (data.errorType === "error-room-not-found") {
+            return APIResponse.fromFailure(data.error, HTTPStatus.NOT_FOUND);
+        } else if (data.errorType === "error-not-allowed") {
+            return APIResponse.fromFailure(data.error, HTTPStatus.UNAUTHORIZED);
+        } else {
+            return APIResponse.fromFailure(data.error, HTTPStatus.BAD_REQUEST);
+        }
     });
 });
