0a3a2b25b7516489fc6cddd97519255a7039ce68
Romain T <romain-thd+github@protonmail.com>
2021-07-22 10:55:26 +0200

    fix: Messages de debug

--- src/helper/envConfig.ts
+++ src/helper/envConfig.ts
@@ -20,7 +20,7 @@ abstract class envConfig {
         // Secret de session
         if (!process.env.hasOwnProperty("SESSION_SECRET") || process.env.SESSION_SECRET === "") {
             process.env.SESSION_SECRET = randomBytes(64).toString("hex");
-            if (process.env.RELEASE_ENVIRONMENT === "prod") {
+            if (process.env.RELEASE_ENVIRONMENT !== "prod") {
                 console.info("Session secret:", process.env.SESSION_SECRET);
             }
         }
@@ -28,7 +28,7 @@ abstract class envConfig {
         // Secret JWT
         if (!process.env.hasOwnProperty("JWT_SECRET") || process.env.JWT_SECRET === "") {
             process.env.JWT_SECRET = randomBytes(64).toString("hex");
-            if (process.env.RELEASE_ENVIRONMENT === "prod") {
+            if (process.env.RELEASE_ENVIRONMENT !== "prod") {
                 console.info("JWT secret:", process.env.JWT_SECRET);
             }
         }
@@ -36,7 +36,7 @@ abstract class envConfig {
         // Secret clé AES
         if (!process.env.hasOwnProperty("AES_KEY") || process.env.AES_KEY === "") {
             process.env.AES_KEY = randomBytes(16).toString("hex");
-            if (process.env.RELEASE_ENVIRONMENT === "prod") {
+            if (process.env.RELEASE_ENVIRONMENT !== "prod") {
                 console.info("AES key:", process.env.AES_KEY);
             }
         }
@@ -44,7 +44,7 @@ abstract class envConfig {
         // Secret IV AES
         if (!process.env.hasOwnProperty("AES_IV") || process.env.AES_IV === "") {
             process.env.AES_IV = randomBytes(8).toString("hex");
-            if (process.env.RELEASE_ENVIRONMENT === "prod") {
+            if (process.env.RELEASE_ENVIRONMENT !== "prod") {
                 console.info("AES IV:", process.env.AES_IV);
             }
         }
